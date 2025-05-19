5b7c6efb62ed30584151f67ab435fa63f1b99dbc
Ryker Fish <rfish@mines.edu>
2025-05-19 17:08:54 -0600

    pull some code from the interface into helpers

--- src/GaussianPuff.py
+++ src/GaussianPuff.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 
 from FastGaussianPuff import CGaussianPuff as fGP
+from FastGaussianPuff import interface_helpers as ih
+
 
 class GaussianPuff:
 
@@ -102,18 +104,18 @@ class GaussianPuff:
                if True, outputs extra information about the simulation and its progress.
         """
 
-        self.obs_dt = obs_dt 
-        self.sim_dt = sim_dt 
+        ih._check_timestep_parameters(sim_dt, puff_dt, obs_dt)
+
+        self.obs_dt = obs_dt
+        self.sim_dt = sim_dt
         self.puff_dt = puff_dt
         if output_dt is None:
             self.out_dt = self.obs_dt
         else:
             self.out_dt = output_dt
 
-        self._check_timestep_parameters()
-
-        self.sim_start = self._ensure_utc(simulation_start)
-        self.sim_end = self._ensure_utc(simulation_end)
+        self.sim_start = ih._ensure_utc(simulation_start)
+        self.sim_end = ih._ensure_utc(simulation_end)
 
         try:
             time_zone = ZoneInfo(time_zone)
@@ -133,37 +135,61 @@ class GaussianPuff:
         if exp_threshold_tolerance is None:
             if unsafe:
                 self.exp_threshold_tolerance = 1e-5
-            else: 
+            else:
                 self.exp_threshold_tolerance = 1e-7
         else:
             self.exp_threshold_tolerance = exp_threshold_tolerance
 
-        if(skip_low_wind):
-            if(low_wind_cutoff <= 0):
-                raise ValueError("[FastGaussianPuff] low wind cutoff must be greater than 0")
+        if skip_low_wind:
+            if low_wind_cutoff <= 0:
+                raise ValueError(
+                    "[FastGaussianPuff] low wind cutoff must be greater than 0"
+                )
             self.skip_low_wind = True
             self.low_wind_cutoff = low_wind_cutoff
 
-        ns = (self.sim_end-self.sim_start).total_seconds()
-        self.n_obs = floor(ns/obs_dt) + 1 # number of observed data points we have
+        ns = (self.sim_end - self.sim_start).total_seconds()
+        self.n_obs = floor(ns / obs_dt) + 1  # number of observed data points we have
 
-        arrays = self._check_array_dtypes(wind_speeds, wind_directions, source_coordinates, 
-                                     emission_rates, grid_coordinates, sensor_coordinates)
-        wind_speeds, wind_directions, source_coordinates, emission_rates, grid_coordinates, sensor_coordinates = arrays
+        arrays = ih._check_array_dtypes(
+            wind_speeds,
+            wind_directions,
+            source_coordinates,
+            emission_rates,
+            grid_coordinates,
+            sensor_coordinates,
+        )
+        (
+            wind_speeds,
+            wind_directions,
+            source_coordinates,
+            emission_rates,
+            grid_coordinates,
+            sensor_coordinates,
+        ) = arrays
 
-        self._check_wind_data(wind_speeds, skip_low_wind)
+        ih._check_wind_data(wind_speeds, skip_low_wind)
 
         # resample the wind data from obs_dt to the simulation resolution sim_dt
-        self._interpolate_wind_data(wind_speeds, wind_directions, puff_dt, self.sim_start, self.sim_end)
+        self.wind_speeds_sim, self.wind_directions_sim = ih._interpolate_wind_data(
+            wind_speeds,
+            wind_directions,
+            puff_dt,
+            simulation_start,
+            simulation_end,
+            self.n_obs,
+        )
 
         # save timeseries of simulation resolution so we can resample back to observation later
-        self.time_stamps_sim = pd.date_range(self.sim_start, self.sim_end, freq=str(self.sim_dt)+"s")
-        self.n_sim = len(self.time_stamps_sim) # number of simulation time steps
+        self.time_stamps_sim = pd.date_range(
+            self.sim_start, self.sim_end, freq=str(self.sim_dt) + "s"
+        )
+        self.n_sim = len(self.time_stamps_sim)  # number of simulation time steps
 
-        source_coordinates = self._parse_source_coords(source_coordinates)
+        source_coordinates = ih._parse_source_coords(source_coordinates)
 
         if puff_duration == None:
-            puff_duration = self.n_sim # ensures we don't overflow time index
+            puff_duration = self.n_sim  # ensures we don't overflow time index
 
         # creates grid
         if not using_sensors:
@@ -172,7 +198,7 @@ class GaussianPuff:
             self.nx = nx
             self.ny = ny
             self.nz = nz
-            self.N_points = self.nx*self.ny*self.nz
+            self.N_points = self.nx * self.ny * self.nz
 
             x_min = grid_coordinates[0]
             y_min = grid_coordinates[1]
@@ -181,9 +207,15 @@ class GaussianPuff:
             y_max = grid_coordinates[4]
             z_max = grid_coordinates[5]
 
-            x, y, z = np.linspace(x_min, x_max, self.nx), np.linspace(y_min, y_max, self.ny), np.linspace(z_min, z_max, self.nz)
+            x, y, z = (
+                np.linspace(x_min, x_max, self.nx),
+                np.linspace(y_min, y_max, self.ny),
+                np.linspace(z_min, z_max, self.nz),
+            )
 
-            self.X, self.Y, self.Z = np.meshgrid(x, y, z) # x-y-z grid across site in utm
+            self.X, self.Y, self.Z = np.meshgrid(
+                x, y, z
+            )  # x-y-z grid across site in utm
             self.grid_dims = np.shape(self.X)
 
             # work with the flattened grids
@@ -243,203 +275,14 @@ class GaussianPuff:
             )
 
         # initialize the final simulated concentration array
-        self.ch4_sim = np.zeros((self.n_sim, self.N_points)) # simulation in sim_dt resolution, flattened
-
-    def _parse_source_coords(self, source_coordinates):
-        size = np.shape(source_coordinates)
-        if len(size) == 1:
-            if size[0] == 3:
-                source_coordinates = np.array(
-                    [source_coordinates]
-                )  # now a nested array- C++ code expects this format
-            else:
-                print(
-                    "[fGP] Error: source_coordinates must be a 3-element array, e.g. [x0, y0, z0]."
-                )
-                exit(-1)
-        else:
-            if size[0] == 1 and size[1] == 3:
-                return source_coordinates
-            elif size[0] > 1 and size[1] == 3:
-                raise (
-                    NotImplementedError(
-                        "[fGP] Error: Multi-source currently isn't implemented. Only provide coordinates for a single source, e.g. [x0, y0, z0] or [[x0, y0, z0]]."
-                    )
-                )
-
-        return source_coordinates
-
-    def _check_timestep_parameters(self):
-
-        # rationale: negative time seems bad. maybe ask a physicist.
-        if self.sim_dt <= 0:
-            print("[fGP] Error: sim_dt must be a positive value")
-            exit(-1)
-
-        # rationale: breaking this would mean multiple puffs are emitted in between simulation time steps. this constraint
-        # decouples sim_dt and puff_dt so that sim_dt can be scaled according to wind speed and puff_dt can be scaled
-        # according to the change in wind direction over time to guarantee accuracy of the simulation (i.e. no skipping)
-        if self.puff_dt < self.sim_dt:
-            print("[fGP] Error: puff_dt must be greater than or equal to sim_dt")
-            exit(-1)
-
-        # rationale: concentration arrays are build at sim_dt resolution. puff_dt = n*sim_dt for an integer n > 0
-        # ensure that puffs are emitted on simulation time steps and prevents the need for a weird interpolation/rounding.
-        # this constaint could likely be avoided, but it isn't that strong and makes the code easier.
-        eps = 1e-5
-        ratio = self.puff_dt/self.sim_dt
-        if abs(ratio - round(ratio)) > eps:
-            print("[fGP] Error: puff_dt needs to be a positive integer multiple of sim_dt")
-            exit(-1)
-
-        # rationale: we don't have simulation data at a resolution less than sim_dt, so you'll have blank
-        # concentration fields if this condition isn't met
-        if self.out_dt < self.sim_dt:
-            print("[fGP] Error: output_dt must be greater than or equal to sim_dt")
-            exit(-1)
-
-    def _check_array_dtypes(self, wind_speeds, wind_directions, source_coordinates, emission_rates, grid_coordinates, sensor_coordinates):
-        variables = {
-            "wind_speeds": wind_speeds,
-            "wind_directions": wind_directions,
-            "source_coordinates": source_coordinates,
-            "emission_rates": emission_rates,
-            "grid_coordinates": grid_coordinates,
-            "sensor_coordinates": sensor_coordinates,
-        }
-
-        casted_arrays = {}
-
-        for name, var in variables.items():
-            if var is not None:
-                try:
-                    casted_arrays[name] = np.asarray(var, dtype=float)
-                except Exception as e:
-                    raise ValueError(f"[fGP] Error: Failed to cast '{name}' to a NumPy float array: {e}. Try using an array dtype.")
-            else:
-                casted_arrays[name] = None
-
-        return (casted_arrays["wind_speeds"], 
-                casted_arrays["wind_directions"], 
-                casted_arrays["source_coordinates"], 
-                casted_arrays["emission_rates"], 
-                casted_arrays["grid_coordinates"], 
-                casted_arrays["sensor_coordinates"])
-
-    def _check_wind_data(self, ws, skip_low_wind):
-        if skip_low_wind:
-            return
-
-        if np.any(ws <= 0):
-            raise( ValueError("[FastGaussianPuff] wind speeds must be greater than 0"))
-        if np.any(ws < 1e-2):
-            print("[FastGaussianPuff] WARNING: There's a wind speed < 0.01 m/s. This is likely a mistake and will cause slow performance. The simulation will continue, but results will be poor as the puff model is degenerate in low wind speeds.")
-
-    def _ensure_utc(self, dt):
-        """
-        Ensures the input datetime is timezone-aware and in UTC.
-        Converts to UTC if it's in a different timezone.
-        """
-        ts = pd.Timestamp(dt)
-
-        if ts.tz is None:
-            raise ValueError(
-                f"[FastGaussianPuff] Naive datetime detected: {dt}. Please provide a timezone-aware datetime."
-            )
-
-        if ts.tz != datetime.timezone.utc:
-            ts = ts.tz_convert("UTC")
-
-        return ts
-
-    def _interpolate_wind_data(self, wind_speeds, wind_directions, puff_dt, sim_start, sim_end):
-        '''
-        Resample wind_speeds and wind_directions to the simulation resolution by interpolation.
-        Inputs:
-            sim_dt [s] (int): 
-                the target time resolution to resample to
-            sim_start, sim_end (pd.DateTime)
-                DateTimes the simulation start and ends at
-            n_obs (int)
-                number of observation data points across the simulation time range
-        '''
-
-        # creates a timeseries at obs_dt resolution
-        time_stamps = pd.date_range(sim_start, sim_end, self.n_obs)
-
-        # technically need a final value to interpolate between, so just extend timeseries
-        if len(wind_speeds) == self.n_obs - 1:
-            wind_speeds = np.append(wind_speeds, wind_speeds[-1])
-
-        if len(wind_directions) == self.n_obs - 1:
-            wind_directions = np.append(wind_directions, wind_directions[-1])
-
-        # interpolate for wind_speeds and wind_directions:
-        ## 1. convert ws & wd to x and y component of wind (denoted by u, v, respectively)
-        ## 2. interpolate u and v
-        ## 3. bring resampled u and v back to resampled ws and wd
-        wind_u, wind_v = self._wind_vector_convert(wind_speeds, 
-                                                    wind_directions,
-                                                    direction = 'ws_wd_2_u_v')
-
-        # resamples wind data to sim_dt resolution
-        wind_df = pd.DataFrame(data = {'wind_u' : wind_u,
-                                    'wind_v' : wind_v}, 
-                            index = time_stamps)
-
-        wind_df = wind_df.resample(str(puff_dt)+'s').interpolate()
-        wind_u = wind_df['wind_u'].to_list()
-        wind_v = wind_df['wind_v'].to_list()
-
-        self.wind_speeds_sim, self.wind_directions_sim = self._wind_vector_convert(wind_u, wind_v,direction= 'u_v_2_ws_wd') 
-
-    def _wind_vector_convert(self, input_1, input_2, direction = 'ws_wd_2_u_v'):
-        '''
-        Convert between (ws, wd) and (u,v)
-        Inputs:
-            input_1: 
-                - list of wind speed [m/s] if direction = 'ws_wd_2_u_v'
-                - list of the x component (denoted by u) of a wind vector [m/s] if direction = 'u_v_2_ws_wd'
-            input_2: 
-                - list of wind direction [degree] if direction = 'ws_wd_2_u_v'
-                - list of the y component (denoted by v) of a wind vector [m/s] if direction = 'u_v_2_ws_wd'
-            direction: 
-                - 'ws_wd_2_u_v': convert wind speed and wind direction to x,y components of a wind vector
-                - 'u_v_2_ws_wd': convert x,y components of a wind vector to wind speed and wind directin
-                       
-        Outputs:
-            quantities corresponding to the conversion direction
-        '''
-        if direction == 'ws_wd_2_u_v':
-            ws, wd = input_1, input_2
-            thetas = [270-x for x in wd] # convert wind direction to the angle between the wind vector and positive x-axis
-            thetas = np.radians(thetas)
-            u = [np.cos(theta)*l for l,theta in zip(ws, thetas)]
-            v = [np.sin(theta)*l for l,theta in zip(ws, thetas)]
-            output_1, output_2 = u, v
-        elif direction == 'u_v_2_ws_wd':
-            u, v = input_1, input_2
-            ws = [(x**2 + y**2)**(1/2) for x,y in zip(u,v)]
-            thetas = [np.arctan2(y, x) for x,y in zip(u,v)] # the angles between the wind vector and positive x-axis
-            thetas = [x * 180 / np.pi for x in thetas]
-            wd = [270 - x for x in thetas] # convert back to cardinal definition
-            for i, x in enumerate(wd):
-                if x < 0:
-                    wd[i] = wd[i] + 360 
-                elif x >= 360:
-                    wd[i] = wd[i] - 360 
-                else:
-                    pass
-            output_1, output_2 = ws, wd
-        else:
-            raise NotImplementedError(">>>>> wind vector conversion direction")
-
-        return output_1, output_2
+        self.ch4_sim = np.zeros(
+            (self.n_sim, self.N_points)
+        )  # simulation in sim_dt resolution, flattened
 
     def _model_info_print(self):
-        '''
+        """
         Print the parameters used in this model
-        '''
+        """
 
         print("\n************************************************************")
         print("****************     PUFF SIMULATION START     *************")
@@ -452,15 +295,17 @@ class GaussianPuff:
         if self.using_sensors:
             print("         Running in sensor mode")
         else:
-            print(f"         Running in grid mode with grid dimensions {self.grid_dims}")
+            print(
+                f"         Running in grid mode with grid dimensions {self.grid_dims}"
+            )
 
     def simulate(self):
-        '''
+        """
         Main code for simulation
         Outputs:
-            ch4_sim_res [ppm] (2-D np.array, shape = [N_t_obs, N_sensor]): 
+            ch4_sim_res [ppm] (2-D np.array, shape = [N_t_obs, N_sensor]):
                 simulated concentrations resampled according to observation dt
-        '''
+        """
         if self.quiet == False:
             self._model_info_print()
 
@@ -476,29 +321,29 @@ class GaussianPuff:
 
         return self.ch4_obs
 
-    def _resample_simulation(self, c_matrix, resample_dt, mode = 'mean'):
-        '''
-        Resample the simulation results 
+    def _resample_simulation(self, c_matrix, resample_dt, mode="mean"):
+        """
+        Resample the simulation results
         Inputs:
-            c_matrix [ppm] (2D np.ndarray, shape = [N_t_sim, self.N_points]): 
+            c_matrix [ppm] (2D np.ndarray, shape = [N_t_sim, self.N_points]):
                 the simulation results in sim_dt resolution across the whole grid
-            dt [s] (scalar, float): 
+            dt [s] (scalar, float):
                 the target time resolution
             mode (str):
-                - 'mean': resmple by taking average 
+                - 'mean': resmple by taking average
                 - 'resample': resample by taking every dt sample
         Outputs:
-            c_matrix_res [ppm] (4D np.array, shape = [N_t_new, self.grid_dims)]): 
-                resampled simulation results 
-        '''
+            c_matrix_res [ppm] (4D np.array, shape = [N_t_new, self.grid_dims)]):
+                resampled simulation results
+        """
 
-        df = pd.DataFrame(c_matrix, index = self.time_stamps_sim)
-        if mode == 'mean':
-            df = df.resample(str(resample_dt)+'s').mean()
-        elif mode == 'resample':
-            df = df.resample(str(resample_dt)+'s').asfreq()
+        df = pd.DataFrame(c_matrix, index=self.time_stamps_sim)
+        if mode == "mean":
+            df = df.resample(str(resample_dt) + "s").mean()
+        elif mode == "resample":
+            df = df.resample(str(resample_dt) + "s").asfreq()
         else:
-            raise NotImplementedError(">>>>> sim to obs resampling mode") 
+            raise NotImplementedError(">>>>> sim to obs resampling mode")
 
         c_matrix_res = df.to_numpy()
 
